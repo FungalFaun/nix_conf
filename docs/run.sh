8f421de756f61cccf60ac672a03a02318c14e735
Adrian Heide <adrian.heide@if.no>
2025-07-24 16:40:39 +0200

    Closer dmenu list

--- docs/run.sh
+++ docs/run.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
-itemNum=$(($1*5))
+# https://linuxhandbook.com/awk-string-functions/
 
-# Get lines mentioned for monitor index
-readarray -t arr < <(cat "monitors" \
+itemNum=$(($1 * 5))
+
+# selection=$(cat "monitors" \
+#   | rg "Monitor| at |make|model|availableModes" \
+#   | awk "NR==$itemNum+1, NR==$itemNum+5" \
+#   | awk -F' ' '/availableModes/ { for( i=2; i<=NF; i++ ) print substr($i,0,index($i,"@")-1)}'\
+#   | uniq\
+#   | wofi --dmenu)
+#
+# echo "You selected $selection"
+
+readarray -t list < <(cat "monitors" \
   | rg "Monitor| at |make|model|availableModes" \
   | awk "NR==$itemNum+1, NR==$itemNum+5" \
-  | awk -F' ' '/availableModes/ { for( i=2; i<=NF; i++ ) print $i}')
+  | awk -F' ' '/availableModes/ { 
+      for( i=2; i<=NF; i++ ) 
+        printf "%s\n", $i
+      }')
 
-for res in "${arr[@]}"; do
-  echo "$res"
-done
+printf "%s\n" "${list[@]}"\
+      | awk '{ at=index($1, "@"); res=substr($1, 1, at -1); printf "%s\n", res }'\
+      | sort -ui\
+      | uniq\
+      | wofi --dmenu
+
