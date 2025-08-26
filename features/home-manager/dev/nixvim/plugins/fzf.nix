49cbf80f06881e100f1b8e6e9d5aa4d5938f3bca
Adrian Heide <adrian.heide@if.no>
2025-08-26 15:23:49 +0200

    Keymaps for fzf-lua

--- features/home-manager/dev/nixvim/plugins/fzf.nix
+++ features/home-manager/dev/nixvim/plugins/fzf.nix
@@ -1,5 +1,30 @@
 {
   programs.nixvim.plugins = {
-    fzf-lua.enable = true;
+    fzf-lua = {
+      enable = true;
+      
+      keymaps = {
+        "<leader>/" = {
+          action = "live_grep";
+          options.desc = "Live grep";
+        };
+        "<leader>sd" = {
+          action = "diagnostics_document";
+          options.desc = "Document Diagnostics";
+        };
+        "<leader>sD" = {
+          action = "diagnostics_workspace";
+          options.desc = "Workspace Diagnostics";
+        };
+        "<leader>sh" = {
+          action = "help_tags";
+          options.desc = "Help Pages";
+        };
+        "<leader>sk" = {
+          action = "keymaps";
+          options.desc = "Key maps";
+        };
+      };
+    };
   };
 }
