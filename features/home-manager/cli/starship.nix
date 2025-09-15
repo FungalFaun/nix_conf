09e9794de2351c1d8cec4335e6bb4a5c8249b579
Adrian Heide <adrian.heide@if.no>
2025-09-15 15:51:24 +0200

    Update starship

--- features/home-manager/cli/starship.nix
+++ features/home-manager/cli/starship.nix
@@ -2,14 +2,154 @@
   programs.starship = {
     enable = true;
     settings = let
-      colors = ["#a3aed2" "#769ff0" "#394260" "#212736" "#1d2230"];
+      gruvbox = {
+        fg0 = "#fbf1c7";
+        bg1 = "#3c3836";
+        bg3 = "#665c54";
+        blue = "#458588";
+        aqua = "#689d6a";
+        green = "#98971a";
+        orange = "#d65d0e";
+        purple = "#b16286";
+        red = "#cc241d";
+        yellow = "#d79921";
+      };
     in {
-      git_branch = with builtins; {
+      format = ''
+        [](${gruvbox.orange})\
+        $os\
+        $username
+        [](bg:${gruvbox.yellow} fg:${gruvbox.orange})
+        $directory
+        [](fg:${gruvbox.yellow} bg:${gruvbox.aqua})
+        $git_branch
+        $git_status
+        [](fg:${gruvbox.aqua} bg:${gruvbox.blue})
+        $c
+        $cpp
+        $rust
+        $golang
+        $nodejs
+        $php
+        $java
+        $kotlin
+        $haskell
+        $python
+        [](fg:${gruvbox.blue} bg:${gruvbox.bg3})
+        $docker_context
+        $conda
+        $pixi
+        [](fg:${gruvbox.bg3} bg:${gruvbox.bg1})
+        $time
+        [ ](fg:${gruvbox.bg1})
+        $line_break$character
+      '';
+
+      os = {
+        disabled = false;
+        style = "bg:${gruvbox.orange} fg:${gruvbox.fg0}";
+
+        symbols = {
+          Windows = "󰍲";
+          Ubuntu = "󰕈";
+          SUSE = "";
+          Raspbian = "󰐿";
+          Mint = "󰣭";
+          Macos = "󰀵";
+          Manjaro = "";
+          Linux = "󰌽";
+          Gentoo = "󰣨";
+          Fedora = "󰣛";
+          Alpine = "";
+          Amazon = "";
+          Android = "";
+          Arch = "󰣇";
+          Artix = "󰣇";
+          EndeavourOS = "";
+          CentOS = "";
+          Debian = "󰣚";
+          Redhat = "󱄛";
+          RedHatEnterprise = "󱄛";
+          Pop = "";
+        };
+      };
+
+      username = {
+        show_always = true;
+        style_user = "bg:${gruvbox.orange} fg:${gruvbox.fg0}";
+        style_root = "bg:${gruvbox.orange} fg:${gruvbox.fg0}";
+        format = "[ $user ]($style)";
+      };
+
+      directory = {
+        style = "fg:${gruvbox.fg0} bg:${gruvbox.yellow}";
+        format = "[ $path ]($style)";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+      };
+
+      git_branch = {
         symbol = "";
-        style = "bg:${elemAt colors 2}";
+        style = "bg:${gruvbox.aqua}";
         truncation_length = 25;
         only_attached = true;
-        format = "[[ $symbol $branch ](fg:${elemAt colors 1} bg:${elemAt colors 2})]($style)";
+        format = "[[($all_status$ahead_behind )](fg:${gruvbox.fg0} bg:${gruvbox.aqua})]($style)";
+      };
+
+      git_status = {
+        style = "bg:${gruvbox.aqua}";
+        format = "[[($all_status$ahead_behind )](fg:${gruvbox.fg0} bg:${gruvbox.aqua})]($style)";
+      };
+
+      nodejs = {
+        symbol = "";
+        style = "bg:${gruvbox.blue}";
+        format = "[[ $symbol( $version) ](fg:${gruvbox.fg0} bg:${gruvbox.blue})]($style)";
+      };
+
+      rust = {
+        symbol = "";
+        style = "bg:${gruvbox.blue}";
+        format = "[[ $symbol( $version) ](fg:${gruvbox.fg0} bg:${gruvbox.blue})]($style)";
+      };
+
+      golang = {
+        symbol = "";
+        style = "bg:${gruvbox.blue}";
+        format = "[[ $symbol( $version) ](fg:${gruvbox.fg0} bg:${gruvbox.blue})]($style)";
+      };
+
+      python = {
+        symbol = "";
+        style = "bg:${gruvbox.blue}";
+        format = "[[ $symbol( $version) ](fg:${gruvbox.fg0} bg:${gruvbox.blue})]($style)";
+      };
+
+      docker_context = {
+        symbol = "";
+        style = "bg:${gruvbox.bg3}";
+        format = "[[ $symbol( $context) ](fg:#83a598 bg:${gruvbox.bg3})]($style)";
+      };
+      
+      time = {
+        disabled = false;
+        time_format = "%R";
+        style = "bg:${gruvbox.bg1}";
+        format = "[[  $time ](fg:${gruvbox.fg0} bg:${gruvbox.bg1})]($style)";
+      };
+
+      line_break = {
+        disabled = false;
+      };
+
+      character = {
+        disabled = false;
+        success_symbol = "[](bold fg:${gruvbox.green})";
+        error_symbol = "[](bold fg:${gruvbox.red})";
+        vimcmd_symbol = "[](bold fg:${gruvbox.green})";
+        vimcmd_replace_one_symbol = "[](bold fg:${gruvbox.purple})";
+        vimcmd_replace_symbol = "[](bold fg:${gruvbox.purple})";
+        vimcmd_visual_symbol = "[](bold fg:${gruvbox.yellow})";
       };
     };
   };
