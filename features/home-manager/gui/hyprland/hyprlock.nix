ab8c179b686ffa8d7c7c1b81db03b8bd1e3aac8b
Adrian Heide <adrian.heide@if.no>
2025-09-30 11:35:25 +0200

    Hyprlock test

--- features/home-manager/gui/hyprland/hyprlock.nix
+++ features/home-manager/gui/hyprland/hyprlock.nix
@@ -2,11 +2,29 @@
   config,
   lib,
   ...
-}:{
+}: let 
+  flakePath = "${config.xdg.configHome}/home-manager";
+  imagePath = "${flakePath}/assets/wallpaper/${config.wallpaper}";
+in {
   programs.hyprlock = {
     enable = true;
     settings = {
       general.hide_cursor = false;
+
+      background = {
+        path = imagePath;
+        blur_passes = 4;
+      };
+
+      input-field = {
+        position = "0, 20%";
+        halign = "center";
+        valign = "center";
+      };
+
+      label = {
+        text = "$TIME";
+      };
     };
   };
 
