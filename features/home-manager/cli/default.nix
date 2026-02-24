e4e2643903f63318987c0faeaa9dc03bd2f0906b
Heide, Adrian <adrian.heide@if.no>
2026-02-24 16:53:09 +0100

    Test things

--- features/home-manager/cli/default.nix
+++ features/home-manager/cli/default.nix
@@ -1,4 +1,45 @@
 {
+  pkgs,
+  config,
+  lib,
+  ...
+}: let 
+  sshStartup = pkgs.writeShellScriptBin "ssh-agent-startup" ''
+#!/bin/bash
+export SSH_ASKPASS="/usr/bin/ksshaskpass"
+
+if ! pgrep -u $USER ssh-agent > /dev/null; then
+    ssh-agent > ~/.ssh-agent-info
+fi
+if [[ "$SSH_AGENT_PID" == "" ]]; then
+    eval $(<~/.ssh-agent-info)
+fi
+  '';
+
+  sshShutdown = pkgs.writeShellScriptBin "ssh-agent-shutdown" ''
+#!/bin/sh
+[ -z "$SSH_AGENT_PID" ] || eval "$(ssh-agent -k)"
+  '';
+
+  sshAutoStart = pkgs.writeShellScriptBin "ssh-autostart-plasma" ''
+#!/bin/bash
+
+# Wait for kwallet
+kwallet-query -l kdewallet > /dev/null
+
+for KEY in $(ls $HOME/.ssh/id_rsa_* | grep -v \.pub); do
+  ssh-add -q $\{KEY} </dev/null
+done
+
+for KEY in $(ls $HOME/.ssh/id_ed25519_* | grep -v \.pub); do
+  ssh-add -q $/{KEY} </dev/null
+done
+  '';
+
+  testScript = pkgs.writeShellScriptBin "test-script.sh" ''
+    echo "This is a test script"
+  '';
+in {
   imports = [
     ./bat.nix
     ./eza.nix
@@ -19,5 +60,11 @@
     ./yazi
 #    ./nixvim
   ];
-  
+
+  # home.packages = [
+  #   sshStartup
+  #   sshShutdown
+  #   sshAutoStart
+  #   testScript
+  # ];
 }
--- features/nixos/common/default.nix
+++ features/nixos/common/default.nix
@@ -14,4 +14,30 @@
     ./swayosd-server.nix
     ./usb.nix
   ];
+
+  # for KDE Plasma
+  # https://unix.stackexchange.com/questions/724894/how-do-i-set-up-kde-kwallet-and-ssh-so-im-not-asked-to-enter-my-ssh-passphrase
+  systemd.services = {
+    ssh-agent = {
+      description = "SSH key agent";
+      wantedBy = "default.target";
+      
+      serviceConfig = {
+        Type = "simple";
+        Environment = "SSH_AUTH_SOCK=%t/ssh-agent.socket";
+        ExecStart = "/usr/bin/ssh-agent -D -a $SSH_AUTH_SOCK";
+      };
+    };
+  };
+
+# [Unit]
+# Description=SSH key agent
+
+# [Service]
+# Type=simple
+# Environment=SSH_AUTH_SOCK=%t/ssh-agent.socket
+# ExecStart=/usr/bin/ssh-agent -D -a $SSH_AUTH_SOCK
+
+# [Install]
+# WantedBy=default.target
 }
